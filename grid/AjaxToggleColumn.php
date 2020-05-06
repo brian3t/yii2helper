08b3b33ba1cea86a55451af60584f13cfc2b3ffd
brian3t <ngxtri@gmail.com>
2020-05-05 20:29:23 -0700

    upgrade system. upgrade to bootstrap 4. Remember to set params.php to have     'bsDependencyEnabled' => false,

---
+++ grid/AjaxToggleColumn.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace usv\yii2helper\grid;
+
+use yii\base\Model;
+use yii\grid\Column;
+
+/**
+ * Class AjaxToggleColumn
+ * An ajax column to be used within Kartik-grid or Yii2-grid
+ * Toggle to change from 0 to 1 and vice versa
+ * usage: ['attribute'=>'attr','class'=>'usv\yii2helper\grid\AjaxToggleColumn']
+ * @package usv\yii2helper\grid
+ */
+class AjaxToggleColumn extends Column
+{
+    public $attribute;
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+        $this->header = ucwords($this->attribute ?? 'Please pass attribute');
+    }
+
+    protected function renderDataCellContent($model, $key, $index)
+    {
+        /** @var $model Model */
+        try {
+            $class_name = strtolower((new \ReflectionClass($model))->getShortName());
+        } catch (\ReflectionException $e) {
+            $class_name = 'error';
+        }
+        return "a button here ";
+    }
+}
