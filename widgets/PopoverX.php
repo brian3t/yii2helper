8538d56b21f48117c04f02a550be621eb6e071a7
brian3t <ngxtri@gmail.com>
2020-05-10 20:26:42 -0700

    WIP: yii2helper PopoverX.php, inherits Kartik PopoverX with minor fix

---
+++ widgets/PopoverX.php
@@ -0,0 +1,22 @@
+<?php
+namespace usv\yii2helper\widgets;
+class PopoverX extends \kartik\popover\PopoverX
+{
+    /**
+     * Registers the needed assets
+     * BN 5/10/20 uncomment $this->toggleButton, so that events are registered using
+     *         'pluginEvents' => [
+    "click.target.popoverX"=>'function() { console.log("click.target.popoverX"); }',
+    "load.complete.popoverX"=>'function() { log("load.complete.popoverX"); }',
+    ]
+     */
+    public function registerAssets()
+    {
+        $view = $this->getView();
+        \kartik\popover\PopoverXAsset::register($view);
+//        if ($this->toggleButton === null) {
+            $this->registerPlugin($this->pluginName);
+//        }
+    }
+
+}
