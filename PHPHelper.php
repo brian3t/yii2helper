5c7e5fe8cf2fb34413853e1cf8c89db9830b91a9
brian3t <ngxtri@gmail.com>
2020-08-02 09:03:30 -0700

    tweak haversin a bit

--- PHPHelper.php
+++ PHPHelper.php
@@ -9,6 +9,8 @@ namespace usv\yii2helper;
  */
 class PHPHelper
 {
+    public static $K_NON_SQL_PARAMS = ['expand', 'sort', 'direction','per-page'];
+
     /**
      * Implode an array, accepting null array as an input
      * @param mixed $array
@@ -44,7 +46,7 @@ class PHPHelper
     public static function arrayKeyToUnderscore($a = null)
     {
         $tmp = [];
-        if (is_null($a) && !is_array($a) && !is_object($a)) {
+        if (is_null($a) && ! is_array($a) && ! is_object($a)) {
             return $tmp;
         }
         foreach ($a as $k => $v) {
@@ -92,7 +94,7 @@ class PHPHelper
             $part = trim($part);
         });
         $state_zip = array_pop($parts);
-        if (!preg_match("/$state\s+$zip" . '$' . "/i", $state_zip, $matches)) {
+        if (! preg_match("/$state\s+$zip" . '$' . "/i", $state_zip, $matches)) {
             return [];
         }
         if (sizeof($matches) < 2) {
@@ -103,11 +105,69 @@ class PHPHelper
         $city = array_pop($parts);
         $address1 = array_pop($parts);
         $address2 = array_pop($parts);
-        if (!empty($address2)) {
+        if (! empty($address2)) {
             [$address1, $address2] = [$address2, $address1];
         }
 
         return compact('address1', 'address2', 'city', 'state', 'zip');
     }
+
+    public static function starts_with($haystack, $needle)
+    {
+        return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
+    }
+
+    public static function ends_with($haystack, $needle)
+    {
+        return substr_compare($haystack, $needle, -strlen($needle)) === 0;
+    }
+
+    /**
+     * Returns max delta in lat/lng, based on mile distance
+     * @param $mile
+     * @return float
+     */
+    public static function rev_haversin_simple($mile)
+    {
+        $MILE_TO_KM = 1.60934;
+        return $mile * $MILE_TO_KM * (1 / 111) * 1.5 / 2; //1km = 1/111 deg ; multiply by .66 to balance off bird-fly vs dog-run ; divide by 2 for radius (half north half south)
+    }
+    /*
+    function distance_to_latlng($dist)
+    {
+        $earth_radius = 3960.0
+$degrees_to_radians = math . pi / 180.0
+$radians_to_degrees = 180.0 / math . pi
+
+def
+def change_in_longitude(latitude, miles):
+    "Given a latitude and a distance west, return the change in longitude."
+    # Find the radius of a circle around the earth at given latitude.
+    r = earth_radius * math . cos(latitude * degrees_to_radians)
+    return (miles / r) * radians_to_degrees
+
+    }
+    function rev_haversin(float $lon, float $lat, float $bearing, int $distance)
+    {
+      "Implementation of the reverse of Haversine formula. Takes one set of latitude/longitude as a start point, a bearing, and a distance, and returns the resultant lat/long pair."
+            [{lon :long lat :lat bearing :bearing distance :distance}]
+        $R = 6378.137; //Radius of Earth in km
+        $lat1 = deg2rad($lat);
+        $lng1 = deg2rad($lon);
+        $angdist = $distance / $R;//angle distance
+        $theta = deg2rad($bearing);
+        //calculate
+        $lat2 = 1;
+        /*
+          (let [R 6378.137
+                lat1 (Math/toRadians lat)
+                lon1 (Math/toRadians lon)
+                angdist (/ distance R)
+                theta (Math/toRadians bearing)
+                lat2 (Math/toDegrees (Math/asin (+ (* (Math/sin lat1) (Math/cos angdist)) (* (Math/cos lat1) (Math/sin angdist) (Math/cos theta)))))
+                lon2 (Math/toDegrees (+ lon1 (Math/atan2 (* (Math/sin theta) (Math/sin angdist) (Math/cos lat1)) (- (Math/cos angdist) (* (Math/sin lat1) (Math/sin lat2))))))]
+            {:lat lat2 :lon lon2}))
+    }
+*/
 
 }
