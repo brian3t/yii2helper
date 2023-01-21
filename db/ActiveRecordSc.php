301cf09f8980549314b4be217ae8b1ea80bcdb28
brian3t <ngxtri@gmail.com>
2023-01-21 09:01:29 -0800

    submodule

---
+++ db/ActiveRecordSc.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Active Record Socal
+ *
+ */
+
+namespace usv\yii2helper\db;
+
+class ActiveRecordSc extends \yii\db\ActiveRecord
+{
+  protected function insertInternal($attributes = null) {
+    if (! $this->beforeSave(true)) {
+      return false;
+    }
+    $values = $this->getDirtyAttributes($attributes);
+    //b3t ignore empty values, let db default handle it
+    foreach ($values as $k => $value) {
+      if (empty($value)) unset($values[$k]);
+    }
+    if (($primaryKeys = static::getDb()->schema->insert(static::tableName(), $values)) === false) {
+      return false;
+    }
+    foreach ($primaryKeys as $name => $value) {
+      $id = static::getTableSchema()->columns[$name]->phpTypecast($value);
+      $this->setAttribute($name, $id);
+      $values[$name] = $id;
+    }
+
+    $changedAttributes = array_fill_keys(array_keys($values), null);
+    $this->setOldAttributes($values);
+    $this->afterSave(true, $changedAttributes);
+
+    return true;
+    //b3t ignore empty values, let db default handle it
+    /* yii code 1/21/23
+     * if (!$this->beforeSave(true)) {
+            return false;
+        }
+        $values = $this->getDirtyAttributes($attributes);
+        if (($primaryKeys = static::getDb()->schema->insert(static::tableName(), $values)) === false) {
+            return false;
+        }
+        foreach ($primaryKeys as $name => $value) {
+            $id = static::getTableSchema()->columns[$name]->phpTypecast($value);
+            $this->setAttribute($name, $id);
+            $values[$name] = $id;
+        }
+
+        $changedAttributes = array_fill_keys(array_keys($values), null);
+        $this->setOldAttributes($values);
+        $this->afterSave(true, $changedAttributes);
+
+        return true;
+     */
+  }
+}
